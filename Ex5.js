e7a810cc3a314d10ea20a97d7c139ccce0318c39
MouzouniDev <aybosuccess@gmail.com>
2020-06-01 10:51:45 +0100

    first commit

---
+++ Ex5.js
@@ -0,0 +1,9 @@
+function bigImg(img) {
+    img.style.width = "40px";
+    img.style.height = "40px";
+}
+
+function smallImg(img) {
+    img.style.width = "30px";
+    img.style.height = "30px";
+}
